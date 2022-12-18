1d4ad1575a060150696aa96836effd2df78a215b
0ardneebwar0 <radhikari.ieu2020@student.ie.edu>
2022-12-18 23:01:15 +0100

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+//import Skull from '../components/Skull.vue'
+import AppAccounts from '../components/AppAccounts.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+{
+    path: '/accounts',
+    name: 'AppAccounts',
+    component: AppAccounts
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
